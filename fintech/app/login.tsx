9af4bd3edae3f0bcb3b997f230257e397499e178
2380492-swansea <163574797+2380492-swansea@users.noreply.github.com>
2025-01-17 19:48:23 +0530

    index_application

---
+++ fintech/app/login.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function login() {
+  return (
+    <View>
+      <Text>likith</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
